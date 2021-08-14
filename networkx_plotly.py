72758e26009cc9f052a5ddf1b79ac403f28bbe34
hxie03 <87540358+hxie03@users.noreply.github.com>
2021-08-14 15:59:16 -0400

    Added dash dropdown menu to show subgraph

--- networkx_plotly.py
+++ networkx_plotly.py
@@ -59,6 +59,91 @@ nx.set_node_attributes(H, nodes.set_index('id')['cpg'].to_dict(), 'cpg')
 nx.set_node_attributes(H, nodes.set_index('id')['pip'].to_dict(), 'pip')
 nx.set_node_attributes(H, nodes.set_index('id')['cpg_pip'].to_dict(), 'cpg_pip')
 
+# set node positions and graph layout
+pos1 = nx.spring_layout(H)
+for node in H.nodes:
+    H.nodes[node]['pos'] = list(pos1[node])
+
+# add edge trace
+edge_x1 = []
+edge_y1 = []
+for edge in H.edges():
+    x0, y0 = H.nodes[edge[0]]['pos']
+    x1, y1 = H.nodes[edge[1]]['pos']
+    edge_x1.append(x0)
+    edge_x1.append(x1)
+    edge_x1.append(None)
+    edge_y1.append(y0)
+    edge_y1.append(y1)
+    edge_y1.append(None)
+
+edge_trace1 = go.Scatter(
+    x=edge_x1, y=edge_y1,
+    line=dict(width=0.5, color='#888'),
+    hoverinfo='none',
+    mode='lines')
+
+# add node trace
+node_x1 = []
+node_y1 = []
+hover1 = []
+for node in H.nodes():
+    x, y = H.nodes[node]['pos']
+    hovertext = "CpG Site(s): " + str(H.nodes[node]['cpg']) + "<br>" + "PIP Score(s): " + str(H.nodes[node]['cpg_pip'])
+    node_x1.append(x)
+    node_y1.append(y)
+    hover1.append(hovertext)
+
+node_trace1 = go.Scatter(
+    x=node_x1, y=node_y1,
+    hovertext = hover1,
+    mode='markers+text',
+    textposition="bottom center",
+    hoverinfo='text',
+    marker=dict(
+        showscale=True,
+        colorscale='YlGnBu',
+        color=[],
+        size=[],
+        colorbar=dict(
+            thickness=15,
+            title='PIP Score',
+            xanchor='left',
+            titleside='right'),
+        ))
+
+# set text as node id
+index1 = 0
+node_text1 = []
+for node in H.nodes():
+    node_text1.append(nodes['id'][index1])
+    index1 = index1 + 1
+node_trace1.text = node_text1
+
+# color node by pip score
+i1 = 0
+col1 = []
+for node in H.nodes():
+  col1.append(nodes['pip'][i1])
+  i1 = i1 + 1
+node_trace1['marker']['color'] = col1
+
+# size node by pip score
+j1 = 0
+size1 = []
+for node in H.nodes():
+  size1.append(nodes['pip'][j1])
+  j1 = j1 + 1
+size_scaled1 = [n * 50 for n in size1]
+node_trace1['marker']['size'] = size_scaled1
+
+fig1 = go.Figure(data=[edge_trace1, node_trace1], layout = go.Layout(showlegend=False, hovermode='closest',
+                        margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
+                        xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                        yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                        height=600,
+                        clickmode='event+select'))
+
 # filter nodes by pip score threshold and create subgraph
 selected_nodes = [n for n,v in H.nodes(data=True) if v['pip'] >= 0.55]
 G = H.subgraph(selected_nodes)
@@ -96,17 +181,12 @@ edge_trace = go.Scatter(
 node_x = []
 node_y = []
 hover = []
-size = []
 for node in G.nodes():
     x, y = G.nodes[node]['pos']
     hovertext = "CpG Site(s): " + str(G.nodes[node]['cpg']) + "<br>" + "PIP Score(s): " + str(G.nodes[node]['cpg_pip'])
-    s = float(G.nodes[node]['pip'])
     node_x.append(x)
     node_y.append(y)
     hover.append(hovertext)
-    size.append(s)
-
-size_scaled = [element * 1000000 for element in size]
 
 node_trace = go.Scatter(
     x=node_x, y=node_y,
@@ -121,7 +201,7 @@ node_trace = go.Scatter(
         size=[],
         colorbar=dict(
             thickness=15,
-            title='Pip Score',
+            title='PIP Score',
             xanchor='left',
             titleside='right'),
         ))
@@ -151,7 +231,7 @@ for node in G.nodes():
 size_scaled = [n * 50 for n in size]
 node_trace['marker']['size'] = size_scaled
 
-fig = go.Figure(data=[edge_trace, node_trace], layout = go.Layout(title='An interactive app for multi-omics gene regulatory network visualization', showlegend=False, hovermode='closest',
+fig = go.Figure(data=[edge_trace, node_trace], layout = go.Layout(showlegend=False, hovermode='closest',
                         margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
                         xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
                         yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
@@ -165,19 +245,33 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash()
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+fig_names = ['Default', '> 0.50']
+fig_dropdown = html.Div([
+    html.H5(children="Filter by PIP Score:"),
+    dcc.Dropdown(
+        id='fig_dropdown',
+        options=[{'label': x, 'value': x} for x in fig_names],
+        value=None
+    )])
+fig_plot = html.Div(id='fig_plot')
+title = html.H1(children='Multioviz')
+subtitle = html.H2(children='An interactive app for multi-omics gene regulatory network visualization')
+app.layout = html.Div([title, subtitle, fig_dropdown, fig_plot])
 
-app.layout = html.Div(children=[
-    html.H1(children='Multioviz'),
+@app.callback(
+dash.dependencies.Output('fig_plot', 'children'),
+[dash.dependencies.Input('fig_dropdown', 'value')])
+def update_output(fig_name):
+    return name_to_figure(fig_name)
 
-    dcc.Graph(
-        id='plotly',
-        figure=fig
-    )
-])
+def name_to_figure(fig_name):
+    figure = fig1
+    if fig_name == 'Default':
+        figure = fig1
+    if fig_name == '> 0.50':
+        figure = fig
+    return dcc.Graph(figure=figure)
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
-    
+app.run_server(debug=True, use_reloader=False)
