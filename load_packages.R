0e83b4e63e1ec88a199c35023ff2cc46a83a4e4a
Ashley Mae Conard <ashleymaeconard@gmail.com>
2023-01-11 22:29:01 -0500

    added devtools installation

--- load_packages.R
+++ load_packages.R
@@ -1,3 +1,4 @@
+install.packages("devtools", repos="http://cran.us.r-project.org", dependencies = TRUE) # nolint
 install.packages("shiny", version = "1.7.1", repos="http://cran.us.r-project.org") # nolint
 install.packages("visNetwork", version = "2.1.0", repos="http://cran.us.r-project.org") # nolint
 install.packages("igraph", version = "1.3.5", repos="http://cran.us.r-project.org") # nolint
@@ -19,3 +20,4 @@ library(shinydashboard)
 library(shinydashboardPlus)
 library(shinyWidgets)
 library(shinyjs)
+library(devtools)
